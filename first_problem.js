b0894fb7d221b30fba9ea96dceb382ae8b452641
tedi178 <tdemo178@gmail.com>
2018-12-21 22:19:50 +0100

    First Problem Solve

---
+++ first_problem.js
@@ -0,0 +1,40 @@
+function myFunction(haystack, needle, threshold) {
+ 
+ 
+    let smallWord = (haystack.length < needle.length) ? haystack : needle
+     
+    let bigWord = (haystack.length > needle.length) ? haystack : needle
+     
+    let mySet = new Set();
+     
+    let count = null;
+     
+     
+    for(let i = 0; i < smallWord.length; i++){
+       
+    for(let j = 0; j <= smallWord.length - threshold; j++){
+         
+    let subNeedle = smallWord.slice(i, threshold + j + i);
+          
+    if(subNeedle.length >= threshold){
+            
+    mySet.add(subNeedle);
+         
+     }  
+       
+    }
+     
+    }
+     
+     
+    for(let item of mySet){
+       
+    count = count + (bigWord.match(item) || []).length;
+     
+    }
+    
+    console.log(count)
+      
+      
+    
+}
