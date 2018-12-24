0fcae29280e6fa27b88423b6b5b94d1c5dfaa963
tedi178 <tdemo178@gmail.com>
2018-12-24 20:32:53 +0100

    Changes in Problem 1 and 2

--- second_problem.js
+++ second_problem.js
@@ -1,30 +1,35 @@
 function myFunction(array) {
- 
   
-    let minValue = Math.min.apply(Math, array);
-      
-    let maxValue = Math.max.apply(Math, array);
-      
-    let minIndex = array.indexOf(minValue);
-      
-    let maxIndex = array.indexOf(maxValue);
-      
-    let sum = null;
-     
-      if(minIndex > maxIndex){
-        
-    let newArray = array.slice(maxIndex+1, minIndex);
-        
-    sum = newArray.reduce((a,b) => a+b)
-      }
-      
-    if(minIndex < maxIndex){
-        
-    let newArray= array.slice(minIndex+1, maxIndex);
-        
-    sum = newArray.reduce((a,b) => a+b)
-      
-    }
-      
-    
-    }
+  let maks=array[0];
+  let min=array[0];
+  let shuma = null;
+  let poz1;
+  let poz2;
+
+for(let i=1; i<array.length ;i++){
+  if(maks <=array[i]){
+    maks=array[i];
+    poz1=i;
+  }
+  
+  if(min>=array[i]){
+    min=array[i];
+    poz2=i;
+  }
+}
+
+if (poz1>poz2){
+  for(let i = poz2+1; i < poz1 ; i++){
+    shuma = shuma + array[i];
+  }
+  console.log("Start Index:"+poz2+    "End Index:"+poz1+   "Sum:"+shuma)  
+} 
+
+if(poz1<poz2){    
+  for(let i=poz1+1;i<poz2;i++){
+    shuma=shuma+array[i];
+  }
+  console.log("Start Index:"+poz1+    "End Index:"+poz2+   "Sum:"+shuma)  
+}
+
+}
